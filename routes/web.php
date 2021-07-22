e096ea84ea6eb07e08d217f06152f74d177b2016
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-07-22 17:40:33 +0200

    Sposto HomeController e modifico const HOME in RouteServiceProvider

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware('auth')->namespace('Admin')->name('admin.')->prefix('admin')->group(function () {
+    Route::get('/', 'HomeController@index')->name('dashboard');
+});

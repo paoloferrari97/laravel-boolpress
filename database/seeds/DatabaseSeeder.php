1a2e75b47c91ae67460341846e2425db3edbcb85
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-07-26 16:42:24 +0200

    Aggiunta chiave esterna a tabella posts

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            PostSeeder::class
+            PostSeeder::class,
+            CategorySeeder::class
         ]);
     }
 }

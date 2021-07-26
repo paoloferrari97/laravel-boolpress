d24da02b93610d0bf3b7dbd9a408a490983174ad
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-07-26 16:34:08 +0200

    Creo colonne tab in migrazione, popolo tabella categorie con il seeder

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Category;
+use Illuminate\Support\Str;
 
 class CategorySeeder extends Seeder
 {
@@ -11,6 +13,13 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $categories = ["Programming", "Automation", "Web Design", "Best Practice"];
+
+        foreach ($categories as $category) { //ciclo nell'array categories
+            $catg = new Category();
+            $catg->name = $category;
+            $catg->slug = Str::slug($category); //per creare slug
+            $catg->save();
+        }
     }
 }

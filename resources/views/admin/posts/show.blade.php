91094f02121fd9cfab525ef809dc2d12f6504889
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-07-26 19:40:35 +0200

    Mostro a schermo la categoria (in pagina show.blade.php)

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -6,6 +6,8 @@
 
     <h1>{{ $post->title }}</h1>
     <p>{{ $post->body }}</p>
+    <h5>Categoria: {{ $post->category ? $post->category->name : 'Non ancora categorizzato' }}</h5>
+    {{-- per mostrare a schermo la categoria (faccio controllo con un operatore ternario, se categoria esiste la mostro, in caso mostro la stringa non ancora categorizzato) --}}
 
     <a href="{{ route('admin.posts.index') }}"><i class="fas fa-arrow-left fa-sm fa-fw"></i> Back</a>
     <a href="{{ route('admin.posts.edit', $post->id) }}">Edit</a>
